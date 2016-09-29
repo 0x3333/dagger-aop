00d0b2f26be6d5b9f66277b900bac44f3d0c1c02
Tercio Gaudencio Filho <terciofilho@gmail.com>
2016-09-29 18:54:56 -0300

    Fixed test.

--- compiler/src/test/resources/unit/InterceptorModule.java
+++ compiler/src/test/resources/unit/InterceptorModule.java
@@ -2,10 +2,15 @@ package test;
 
 import dagger.Binds;
 import dagger.Module;
+import javax.annotation.Generated;
 
 /**
  * This class is the default Dagger module for Intercepted Methods.
  */
+@Generated(
+    value = "com.github.x3333.dagger.aop.internal.InterceptorProcessorStep",
+    comments = "https://github.com/0x3333/dagger-aop"
+)
 @Module
 public abstract class InterceptorModule {
   @Binds
